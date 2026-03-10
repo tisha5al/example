c0ca07381cfaad4e76a853406c71e317f0ad2268
tisha5al <tishapanchal2024@gmail.com>
2026-03-10 16:15:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("hello Tisha panchal");
         System.out.println("Addition: "+(2+4));
+        System.out.println("Addition: "+(2+7));
     }
 }
